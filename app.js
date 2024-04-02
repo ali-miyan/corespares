c41851fd581a9548c879230378bb98e3203606bc
ali <alimiyan1112@gmail.com>
2024-04-02 18:03:30 +0530

    aligned admin-side

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 // Import required modules
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
 const noCache = require('nocache');
-const morgan = require('morgan')
 require('dotenv').config();
+const Config=require('./config/config')
 
 // Set up Express application
 const app = express();
 const port = 3009;
 
-// MongoDB connection configuration
-const dbUrl = process.env.MONGODB_URI;
-
-// Connect to MongoDB
-mongoose.connect(dbUrl);
-
-// Listen for the 'connected' event when MongoDB connection is established
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-// Listen for the 'error' event in case of MongoDB connection errors
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
 
 // Set up static files and view engine
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -32,7 +16,6 @@ app.set('view engine', 'ejs');
 
 
 app.use(noCache());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 // User Routes
 // const userRoute = require('./routes/userroute');
---
+++ config/config.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mini-ecommerce', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      family: 4
+    })
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
+
+
+module.exports = {
+  connectDB
+}
