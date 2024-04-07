a803f83b160b94ec94d90cecf562fbd8fdeb60d2
zayed17 <mozayed9745@gmail.com>
2024-04-07 21:54:28 +0530

    some changes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -39,5 +39,6 @@ adminRoute.delete("/delete-category", admincontrollers.deleteCategory);
 adminRoute.get("/products", productController.loadProducts);
 adminRoute.get("/add-products", productController.loadAddProducts);
 adminRoute.post("/add-product",multer.uploadMultiple, productController.addProductPost);
+adminRoute.post('/category-edit',admincontrollers.categoryEdit);
 
 module.exports = adminRoute;
